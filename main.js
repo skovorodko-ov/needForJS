e5da35a3498e24d4966a9aa6c727f7a49f9171df
skovorodko-ov <81468300+skovorodko-ov@users.noreply.github.com>
2021-06-24 20:11:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,10 +30,10 @@ function getQuantityElements(heightElement){
   return document.documentElement.clientHeight / heightElement + 1;
 }
 
-console.log(getQuantityElements(50));
-
 function startGame(){
   start.classList.add('hide');
+  gameArea.innerHTML = '';
+  
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
@@ -53,8 +53,12 @@ function startGame(){
 
   }
 
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+  car.style.bottom = '10px';
+  car.style.top = 'auto';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -63,6 +67,8 @@ function startGame(){
 function playGame(){
   
   if (setting.start){
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0){
@@ -81,6 +87,7 @@ function playGame(){
 
     car.style.left = setting.x + 'px';
     car.style.top = setting.y + 'px';
+
       requestAnimationFrame(playGame);
   }
 }
@@ -109,7 +116,22 @@ function moveRoad() {
 
 function moveEnemy(){
   let enemy = document.querySelectorAll('.enemy');
+
   enemy.forEach(function(item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+
+    if (carRect.top <= enemyRect.bottom && 
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top) {
+        setting.start = false;
+        console.warn("ДТП");
+        start.classList.remove('hide');
+        start.style.top = score.offsetHeight;
+    }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     
